3e5784d4c56da452612c2fbd0550e9b224a860ee
Miguel Orozco <m68476521.1@gmail.com>
2025-09-27 11:44:02 -0700

    FEATURE: Add platform module
    Develop by Miguel Orozco <morozco@bolt.com>

---
+++ shared/src/commonMain/kotlin/com/m68476521/weather/di/Module.kt
@@ -0,0 +1,16 @@
+package com.m68476521.weather.di
+
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
+import com.m68476521.weather.geolocation.locals.DatabaseFactory
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+expect val platformModule: Module
+
+val sharedModule = module {
+    single {
+        get<DatabaseFactory>().create()
+            .setDriver(BundledSQLiteDriver())
+            .build()
+    }
+}
